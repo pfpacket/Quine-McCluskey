e76d30630dd1457c154ba346baa9abdfb70315d9
equal-l2 <eng.equall2@gmail.com>
2018-06-22 17:46:06 +0900

    Use <regex> instead of <boost/regex.hpp>

--- src/logical_expr.hpp
+++ src/logical_expr.hpp
@@ -9,8 +9,8 @@
 #include <iterator>
 #include <algorithm>
 #include <stdexcept>
+#include <regex>
 #include <cmath>
-#include <boost/regex.hpp>
 #include <boost/format.hpp>
 #include <boost/tokenizer.hpp>
 #include <boost/call_traits.hpp>
@@ -120,14 +120,13 @@ public:
     vector<string> scanner() {
         if( expr_.empty() )
             throw std::runtime_error("expr: Expression is empty, aborted");
-        string expr_with_nospaces = boost::regex_replace(expr_, boost::regex("\\s"), "");
-        boost::regex reg(
+        string expr_with_nospaces = std::regex_replace(expr_, std::regex("\\s"), "");
+        std::regex reg(
             (boost::format("([A-Za-z_-]+)\\((((\\s*[A-Za-z],)*)([A-Za-z]))\\)=(((%1%?[A-Za-z])+\\+)*((%1%?[A-Za-z])+))$") 
-                % (escape ? string{'\\', inverter} : string{inverter})).str(),
-             boost::regex::perl
+                % (escape ? string{'\\', inverter} : string{inverter})).str()
         );
-        boost::smatch result;
-        if( !boost::regex_match(expr_with_nospaces, result, reg) )
+        std::smatch result;
+        if( !std::regex_match(expr_with_nospaces, result, reg) )
             throw std::runtime_error("expr: Input string does not match the correct form");
         func_name_ = result[1];
 //                            decl-vars  decl-terms
